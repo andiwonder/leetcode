7a0871617e9503b9dabff926de2a80df984e6d72
andiwonder <skasliwal12@gmail.com>
2022-04-26 23:30:59 -0400

    valid anagram

---
+++ arrays/valid-anagram/valid-anagram.js
@@ -0,0 +1,29 @@
+var isAnagram = function(s, t) {
+    let sMap = {}
+    let tMap = {};
+
+    countChars(s, sMap);
+    countChars(t, tMap);
+
+    function countChars(s, map) {
+        for(let i = 0; i < s.length; i++) {
+            if(map[s[i]] === undefined) {
+                map[s[i]] = 1;
+            } else {
+                map[s[i]] += 1;
+            }
+        }
+    }
+
+    if(Object.keys(sMap).length !== Object.keys(tMap).length) {
+        return false;
+    }
+
+    for(key of Object.keys(sMap)) {
+        if(sMap[key] !== tMap[key]) {
+            return false;
+        }
+    }
+
+    return true;
+};
