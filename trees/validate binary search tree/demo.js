e038d6683a69e541ecbfb3a829f55e3b4a971c03
Swapnil Kasliwal <swapnilkasliwal@Swapnils-MacBook-Pro.local>
2022-04-26 23:23:52 -0400

    validate bst
    reset author

---
+++ trees/validate binary search tree/demo.js
@@ -0,0 +1,28 @@
+/* 
+
+	the main idea is to perform in-order (left -> root -> right) traversal
+	compare prev to current, should be current > prev
+	not optimal
+
+*/
+
+
+var isValidBST = function(root) {
+  let output = [];
+  
+  function traverse(root) {
+    if(root === null) return;
+    
+    traverse(root.left);
+    output.push(root.val);
+    traverse(root.right);
+  }
+
+  traverse(root);
+  
+  for(let i = 0 ; i < output.length - 1; i++) {
+    if(output[i + 1] <= output[i]) return false;
+  }
+
+  return true;
+};
